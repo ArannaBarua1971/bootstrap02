42b67ba981afc970e26961ba05268ff39c7fccb7
ArannaBarua1971 <arannabaruase71@gmail.com>
2022-09-09 21:40:50 +0600

    new thing

--- js/app.js
+++ js/app.js
@@ -1,4 +1,7 @@
 $(function(){
+    $(window).on('load',function(){
+        $("#preloader").fadeOut(500)
+      })
     $('.slider').slick({
         prevArrow:"<i class=' icon fa-2xl fas fa-chevron-left'></i>",
         nextArrow:'<i class=" icon fa-2xl fas fa-chevron-right"></i>',
